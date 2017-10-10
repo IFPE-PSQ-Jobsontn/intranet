bc1cd8d092e1c171c9940d92e31f830c96c66a9b
Jobson Tenório do Nascimento <1804315@PESQUEIRA.IFPE>
2017-10-10 14:15:07 -0300

    Atualizado em 10/10/2017.

---
+++ resources/lang/pt-br/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Nos desculpe, mas, as suas credenciais de acesso não foram encontradas.',
+    'throttle' => 'Você tentou acessar várias vezes sem sucesso. Por favor tente novamente em :seconds segundos.',
+
+];
