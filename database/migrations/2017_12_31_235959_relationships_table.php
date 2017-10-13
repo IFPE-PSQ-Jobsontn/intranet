81262f8220d152864cafde14f182e97d3c6c19f7
Jobson Tenório do Nascimento <jobson.nascimento@pesqueira.ifpe.edu.br>
2017-10-12 22:28:11 -0300

    Atualizado em 12/10/2017.

--- database/migrations/2017_12_31_235959_relationships_table.php
+++ database/migrations/2017_12_31_235959_relationships_table.php
@@ -25,12 +25,6 @@ class RelationshipsTable extends Migration
             $table->integer('permission_id')->unsigned();
             $table->integer('role_id')->unsigned();
         });
-        // Tabela Pivô - Usuário X Servidor
-        Schema::create('employee_user', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('employee_id')->unsigned();
-            $table->integer('user_id')->unsigned();
-        });
         //Relações N:N - Usuário X Perfil
         Schema::table('role_user', function (Blueprint $table) {
             $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('restrict');
@@ -41,10 +35,9 @@ class RelationshipsTable extends Migration
             $table->foreign('permission_id')->references('id')->on('permissions')->onUpdate('cascade')->onDelete('restrict');
             $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('restrict');
         });
-        //Relações N:N - Usuário X Servidor
-        Schema::table('employee_user', function (Blueprint $table) {
-            $table->foreign('employee_id')->references('id')->on('employees')->onUpdate('cascade')->onDelete('restrict');
-            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('restrict');
+        //Relações 1:N - Tabela de Usuários
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('employee_id')->references('id')->on('employee')->onUpdate('cascade')->onDelete('restrict');
         });
         //Relações 1:N - Tabela de Setores
         Schema::table('departments', function (Blueprint $table) {
@@ -56,6 +49,26 @@ class RelationshipsTable extends Migration
         Schema::table('employees', function (Blueprint $table) {
             $table->foreign('person_id')->references('id')->on('people')->onUpdate('cascade')->onDelete('restrict');
             $table->foreign('departament_id')->references('id')->on('departments')->onUpdate('cascade')->onDelete('restrict');
+        });
+        //Relações 1:N - Tabela de Veículos
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->foreign('species_id')->references('id')->on('denatran_species')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreign('type_id')->references('id')->on('denatran_types')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreign('fuel_id')->references('id')->on('denatran_fuels')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreign('category_id')->references('id')->on('denatran_categories')->onUpdate('cascade')->onDelete('restrict');
+            $table->foreign('color_id')->references('id')->on('denatran_colors')->onUpdate('cascade')->onDelete('restrict');
+        });
+        //Relações 1:N - Tabela de Motoristas
+        Schema::table('drivers', function (Blueprint $table) {
+            $table->foreign('person_id')->references('id')->on('people')->onUpdate('cascade')->onDelete('restrict');
+        });
+        //Relações 1:N - Tabela de Veículos Particulares
+        Schema::table('private_vehicles', function (Blueprint $table) {
+            $table->foreign('person_id')->references('id')->on('people')->onUpdate('cascade')->onDelete('restrict');
+        });
+        //Relações 1:N - Tabela de Pessoas
+        Schema::table('people', function (Blueprint $table) {
+            $table->foreign('registry_entity_uf_id')->references('id')->on('states')->onUpdate('cascade')->onDelete('restrict');
         });
     }
 
@@ -75,6 +88,9 @@ class RelationshipsTable extends Migration
             $table->dropForeign(['permission_id']);
             $table->dropForeign(['role_id']);
         });
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['employee_id']);
+        });
         Schema::table('departments', function (Blueprint $table) {
             $table->dropForeign(['parent']);
             $table->dropForeign(['manager']);
@@ -84,8 +100,25 @@ class RelationshipsTable extends Migration
             $table->dropForeign(['person_id']);
             $table->dropForeign(['departament_id']);
         });
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropForeign(['species_id']);
+            $table->dropForeign(['type_id']);
+            $table->dropForeign(['fuel_id']);
+            $table->dropForeign(['category_id']);
+            $table->dropForeign(['color_id']);
+        });
+        Schema::table('drivers', function (Blueprint $table) {
+            $table->dropForeign(['person_id']);
+        });
+        Schema::table('private_vehicles', function (Blueprint $table) {
+            $table->dropForeign(['person_id']);
+        });
+        Schema::table('people', function (Blueprint $table) {
+            $table->dropForeign(['registry_entity_uf_id']);
+        });
         //Apagar tabelas Pivô
         Schema::dropIfExists('role_user');
         Schema::dropIfExists('permission_role');
+
     }
 }
